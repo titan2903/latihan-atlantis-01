ae0041f66378179d4232ecc16d3dd8a8785f92b8
titan <titanioy98@gmail.com>
2023-10-22 07:37:29 +0700

    add module network

---
+++ module/gcp/network/main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials # login menggunakan json key yang telah di create
+}
+
+provider "google-beta" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials # login menggunakan json key yang telah di create
+}
+
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 7.4"
+
+    project_id   = var.project_id
+    network_name = var.network_name
+
+    subnets = [
+        {
+            subnet_name           = var.subnet_name
+            subnet_ip             = var.subnet_ip
+            subnet_region         = var.subnet_region
+        }
+    ]
+}
---
+++ module/gcp/network/vars.tf
@@ -0,0 +1,32 @@
+variable "project_id" {
+  type        = string
+  description = "isi dengan project id gcp"
+}
+
+variable "subnet_region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "credentials" {
+  type = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_ip" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
